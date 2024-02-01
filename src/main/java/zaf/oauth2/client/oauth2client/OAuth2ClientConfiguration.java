1087bc396427d6cf64aaab98857f7f5dbdf83a3c
adrien zaffran <zaffran.ad@gmail.com>
2024-02-01 21:30:59 +0100

    simplify

--- src/main/java/zaf/oauth2/client/oauth2client/OAuth2ClientConfiguration.java
+++ src/main/java/zaf/oauth2/client/oauth2client/OAuth2ClientConfiguration.java
@@ -3,50 +3,19 @@ package zaf.oauth2.client.oauth2client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.endpoint.DefaultClientCredentialsTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequestEntityConverter;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 @Configuration
 public class OAuth2ClientConfiguration {
-
-    @Bean
-    ClientRegistration myClientRegistration(
-            @Value("${spring.security.oauth2.client.provider.client-example.token-uri}") String token_uri,
-            @Value("${spring.security.oauth2.client.registration.client-example.client-id}") String client_id,
-            @Value("${spring.security.oauth2.client.registration.client-example.client-secret}") String client_secret,
-            @Value("${spring.security.oauth2.client.registration.client-example.authorization-grant-type}") String authorizationGrantType
-    ) {
-        return ClientRegistration
-                .withRegistrationId("client-example")
-                .tokenUri(token_uri)
-                .clientId(client_id)
-                .clientSecret(client_secret)
-                .authorizationGrantType(new AuthorizationGrantType(authorizationGrantType))
-                .build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(ClientRegistration clientRegistration) {
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
-
-    @Bean
-    public OAuth2AuthorizedClientService auth2AuthorizedClientService(ClientRegistrationRepository clientRegistrationRepository) {
-        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
-    }
-
+    
     @Bean
     public OAuth2AuthorizedClientManager authorizedClientManager(
             ClientRegistrationRepository clientRegistrationRepository,
